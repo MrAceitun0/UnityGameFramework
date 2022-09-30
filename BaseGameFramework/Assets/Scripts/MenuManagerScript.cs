d681bb1931528998ce2ed40f380bec4b2988db88
MrAceitun0 <aceitunodev@gmail.com>
2022-09-30 20:04:40 +0200

    First scenes

---
+++ BaseGameFramework/Assets/Scripts/MenuManagerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManagerScript : MonoBehaviour
+{
+
+    public void loadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
