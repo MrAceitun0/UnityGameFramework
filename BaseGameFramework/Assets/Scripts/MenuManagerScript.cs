04faca5098786ab8766fb1c1fbbe5bd26bd6b7cd
MrAceitun0 <aceitunodev@gmail.com>
2022-10-02 19:05:40 +0200

    SplashArt

--- BaseGameFramework/Assets/Scripts/MenuManagerScript.cs
+++ BaseGameFramework/Assets/Scripts/MenuManagerScript.cs
@@ -6,37 +6,26 @@ using System.Collections.Generic;
 
 public class MenuManagerScript : MonoBehaviour
 {
-    private const string VoumeReference = "volume";
+    private const string VOLUME = "volume";
     public AudioMixer masterAudioMixer;
 
     Resolution[] availableResolutions;
     public Dropdown resolutionDropdown;
 
+    private const string IS_LOADING_SCREEN = "isLoadingScreen";
+    public Animator faderAnimator;
+
     private void Start()
     {
-        availableResolutions = Screen.resolutions;
-        List<string> availableResolutionsDisplay = new List<string>();
-
-        int currentResolutionIndex = 0;
-
-        for (int i = 0; i < availableResolutions.Length; i++)
+        if (resolutionDropdown)
         {
-            availableResolutionsDisplay.Add(availableResolutions[i].width + " x " + availableResolutions[i].height);
-            if(availableResolutions[i].width == Screen.currentResolution.width 
-                && availableResolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
+            getAvailableResolutions();
         }
-
-        resolutionDropdown.ClearOptions();
-        resolutionDropdown.AddOptions(availableResolutionsDisplay);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
     }
 
     public void loadScene(string sceneName)
     {
+        faderAnimator.SetBool(IS_LOADING_SCREEN, true);
         SceneManager.LoadSceneAsync(sceneName);
     }
 
@@ -47,7 +36,7 @@ public class MenuManagerScript : MonoBehaviour
 
     public void setVolume(float volume)
     {
-        masterAudioMixer.SetFloat(VoumeReference, volume);
+        masterAudioMixer.SetFloat(VOLUME, volume);
     }
 
     public void setQuality(int qualityIndex)
@@ -63,5 +52,33 @@ public class MenuManagerScript : MonoBehaviour
     public void setResolution(int resolutionIndex)
     {
         Screen.SetResolution(availableResolutions[resolutionIndex].width, availableResolutions[resolutionIndex].height, Screen.fullScreen);
+    }
+
+    public void openWebInBrowser(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    private void getAvailableResolutions()
+    {
+        availableResolutions = Screen.resolutions;
+        List<string> availableResolutionsDisplay = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < availableResolutions.Length; i++)
+        {
+            availableResolutionsDisplay.Add(availableResolutions[i].width + " x " + availableResolutions[i].height);
+            if (availableResolutions[i].width == Screen.currentResolution.width
+                && availableResolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.ClearOptions();
+        resolutionDropdown.AddOptions(availableResolutionsDisplay);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 }
---
+++ BaseGameFramework/Assets/Scripts/SplashArtScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashArtScript : MonoBehaviour
+{
+
+    MenuManagerScript menuManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuManager = FindObjectOfType<MenuManagerScript>();
+        StartCoroutine("goToMenu");
+    }
+
+    private IEnumerator goToMenu()
+    {
+        yield return new WaitForSeconds(4f);
+        menuManager.loadScene("MainMenuScene");
+    }
+}
